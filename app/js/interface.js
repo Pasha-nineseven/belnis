c05c29b5224d72e02937829e523b90f616d54a2c
Pavelbox <pavv.inbox@gmail.com>
2019-09-05 16:17:20 +0300

    text+

--- app/js/interface.js
+++ app/js/interface.js
@@ -35,7 +35,89 @@ $(document).ready(function() {
     //FS
     if ($('.fs').length>0) {
     	$('.fs').styler();
+    };
+
+
+
+    //UPLOAD FILE
+    $("#file-upload").change(function() {
+        var ele = document.getElementById($(this).attr('id'));
+        var result = ele.files;
+        for(var x = 0;x< result.length;x++){
+         var fle = result[x];
+            $("#file-upload-output").append("<div class='file-upload-output__item'>" + fle.name + "<a href='#' class='file-upload-output__close'>Удалить файл" + "</a>" + "</div>");        
+        }
+    });
+    $("body").on("click", ".file-upload-output__close", function(e){
+        e.preventDefault();
+        $(this).parents(".file-upload-output__item").last().remove();
+    });
+
+
+    //ACCORDEON
+    $("body").on("click", ".accordeon__link", function(e){
+        e.preventDefault();
+        $(this).parents('.accordeon__item').toggleClass('active');
+        $(this).next('.accordeon__info').slideToggle();
+    });
+
+
+    //TABS
+    if ($('.resp-tabs').length>0) {
+        $('.resp-tabs').responsiveTabs({
+            startCollapsed: 'accordion'
+        });
     }
+
+
+    //default-slider
+	if ($('.default-slider').length>0) {
+		var $gallery = $('.default-slider');
+
+	    $gallery.slick({
+			speed: 250,
+			swipe: true,
+			swipeToSlide: true,
+			touchThreshold: 10,
+			arrows:true,
+			dots:false,
+			useTransform:true,
+			accessibility: false,
+			infinite: false,
+			slidesToShow: 3,
+  			slidesToScroll: 1,
+  	// 		responsive: [
+			//     {
+			//       breakpoint: 850,
+			//       settings: {
+			//         slidesToShow: 5,
+			//         slidesToScroll: 1,
+			//       }
+			//     },
+			//     {
+			//       breakpoint: 700,
+			//       settings: {
+			//         slidesToShow: 4,
+			//         slidesToScroll: 1,
+			//       }
+			//     },
+			//     {
+			//       breakpoint: 600,
+			//       settings: {
+			//         slidesToShow: 3,
+			//         slidesToScroll: 1,
+			//       }
+			//     },
+			//     {
+			//       breakpoint: 500,
+			//       settings: {
+			//         slidesToShow: 2,
+			//         slidesToScroll: 1,
+			//       }
+			//     },
+			// ]
+	    });
+	};
 });
 
 
